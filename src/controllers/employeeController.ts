ccbfdcb672c755679c9ae844598535acbdf84826
Davi <lobpreal@gmail.com>
2024-03-24 22:10:39 -0300

    created employee and user controller

---
+++ src/controllers/employeeController.ts
@@ -0,0 +1,162 @@
+import { Request, Response } from 'express';
+import { BadRequestError, InvalidFormatError, NotFoundError, ServerError, UnauthorizedError } from '../helpers/api-error';
+import chat from '../chat/statusMessage';
+import { s3Client } from '../services/aws';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import bcrypt from 'bcrypt';
+import { employeeRepository } from '../repositories/employeeRepository';
+import { Employee } from '../entities/Employee';
+import jwt from 'jsonwebtoken';
+import deleteImage from '../services/deleteImage';
+import { userRepository } from '../repositories/userRepository';
+import { envChecker } from '../utils/envChecker';
+import { RequestWhitEntity } from '../interfaces/RequestWhitEntity';
+
+export class EmployeeController {
+  async index(_: Request, res: Response): Promise<Response<any, Record<string, any>>> {
+    const employees: Employee[] = await employeeRepository.find();
+
+    if (employees.length < 1) {
+      throw new NotFoundError(chat.error404);
+    }
+
+    return res.status(200).json(employees);
+  }
+
+  async show(req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
+    const { id } = req.params;
+
+    const employeeId: number = parseInt(id);
+
+    if (!id || isNaN(employeeId)) {
+      throw new BadRequestError(chat.error400);
+    }
+
+    const employee: Employee | null = await employeeRepository.findOne({ where: { id: employeeId } });
+
+    if (!employee) {
+      throw new NotFoundError(chat.error404);
+    }
+    return res.status(200).json(employee);
+  }
+
+  async store(req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
+    const { name, email, password } = req.body;
+    let avatar: Express.Multer.File | undefined = req.file;
+
+    if (!name || !email || !password) {
+      throw new InvalidFormatError(chat.error400);
+    }
+
+    const existingEmployee = await employeeRepository.findByEmail(email);
+    const existingUser = await userRepository.findByEmail(email);
+
+    if (existingEmployee.length > 1 && existingUser.length > 1) {
+      throw new InvalidFormatError(chat.error400);
+    }
+
+    //file upload
+    if (avatar) {
+      const Key = `profiles/employees${encodeURIComponent(name)}`;
+
+      await s3Client.send(
+        new PutObjectCommand({
+          Bucket: envChecker(process.env.KEY_NAME_BUCKET),
+          Key,
+          Body: avatar.buffer,
+          ContentType: avatar.mimetype
+        })
+      );
+
+      const s3URL: string = `http://${envChecker(process.env.ENDPOINT_BUCKET)}/${Key}`;
+
+      const encryptedPassword = await bcrypt.hash(password, 10);
+
+      const newEmployee: Omit<Employee, 'id'> = {
+        name,
+        email,
+        password: encryptedPassword,
+        avatar: s3URL
+      };
+      const savedEmployee: Employee = employeeRepository.create(newEmployee);
+      await employeeRepository.save(savedEmployee);
+
+      return res.status(201).json({ ...newEmployee });
+    } else {
+      const encryptedPassword = await bcrypt.hash(password, 10);
+
+      const newEmployee: Omit<Employee, 'id'> = {
+        name,
+        email,
+        password: encryptedPassword,
+        avatar: ''
+      };
+      const savedEmployee: Employee = employeeRepository.create(newEmployee);
+      await employeeRepository.save(savedEmployee);
+
+      return res.status(201).json({ ...newEmployee });
+    }
+  }
+
+  async login(req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      throw new InvalidFormatError(chat.error400);
+    }
+
+    const employees: Employee[] = await employeeRepository.findByEmail(email);
+
+    if (employees.length < 1) {
+      throw new NotFoundError(chat.error404);
+    }
+
+    const employee: Employee = employees[0];
+
+    const correctPassword: boolean = await bcrypt.compare(password, employee.password);
+
+    if (!correctPassword) {
+      throw new InvalidFormatError(chat.error400);
+    }
+
+    const jwtPass: string | undefined = envChecker(process.env.JWT_PASS);
+
+    const token = jwt.sign({ id: employee.id }, jwtPass, { expiresIn: '8h' });
+
+    const { password: _, ...employeeData } = employee;
+
+    return res.status(200).json({ employee: employeeData, token });
+  }
+
+  async update(req: Request, res: Response) {}
+
+  async destroy(req: RequestWhitEntity, res: Response): Promise<Response<any, Record<string, any>>> {
+    const { id } = req.params;
+
+    const userId: number = parseInt(id);
+
+    if (!id || isNaN(userId)) {
+      throw new InvalidFormatError(chat.error400);
+    }
+
+    const employee: Employee | null = await employeeRepository.findOne({ where: { id: userId } });
+
+    if (!employee) {
+      throw new NotFoundError(chat.error404);
+    }
+
+    const avatar: string | undefined = employee.avatar;
+
+    if (avatar) {
+      await deleteImage(avatar, 'profiles/employees');
+    }
+
+    if (employee.id === req.user.id) {
+      await employeeRepository.delete(employee);
+    } else {
+      throw new UnauthorizedError(chat.error401);
+    }
+
+    return res.status(203).json();
+  }
+}
---
+++ src/controllers/userController.ts
@@ -0,0 +1,155 @@
+import { Request, Response } from 'express';
+import { userRepository } from '../repositories/userRepository';
+import { BadRequestError, InvalidFormatError, NotFoundError, ServerError, UnauthorizedError } from '../helpers/api-error';
+import chat from '../chat/statusMessage';
+import { User } from '../entities/User';
+import { s3Client } from '../services/aws';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import deleteImage from '../services/deleteImage';
+import { employeeRepository } from '../repositories/employeeRepository';
+import { RequestWhitEntity } from '../interfaces/RequestWhitEntity';
+
+export class UserController {
+  async store(req: Request, res: Response) {
+    const { name, email, password } = req.body;
+    let avatar: Express.Multer.File | undefined = req.file;
+
+    if (!name || !email || !password) {
+      throw new InvalidFormatError(chat.error400);
+    }
+
+    const existingEmployee = await employeeRepository.findByEmail(email);
+    const existingUser = await userRepository.findByEmail(email);
+
+    if (existingEmployee.length > 1 && existingUser.length > 1) {
+      throw new InvalidFormatError(chat.error400);
+    }
+
+    //file upload
+    if (avatar) {
+      const Key: string = `profiles/users/${encodeURIComponent(name)}`;
+
+      await s3Client.send(
+        new PutObjectCommand({
+          Bucket: process.env.KEY_NAME_BUCKET,
+          Key,
+          Body: avatar.buffer,
+          ContentType: avatar.mimetype
+        })
+      );
+
+      const s3URL: string = `http://${process.env.ENDPOINT_BUCKET}/${Key}`;
+
+      const encryptedPassword = await bcrypt.hash(password, 10);
+
+      const newUser: Omit<User, 'id'> = {
+        name,
+        email,
+        password: encryptedPassword,
+        avatar: s3URL,
+        adress: []
+      };
+      const savedUser: User = userRepository.create(newUser);
+      await userRepository.save(savedUser);
+
+      return res.status(201).json({ ...newUser });
+    } else {
+      const encryptedPassword = await bcrypt.hash(password, 10);
+
+      const newUser: Omit<User, 'id'> = {
+        name,
+        email,
+        password: encryptedPassword,
+        avatar: '',
+        adress: []
+      };
+      const savedUser: User = userRepository.create(newUser);
+      await userRepository.save(savedUser);
+
+      return res.status(201).json({ ...newUser });
+    }
+  }
+  async show(req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
+    const { id } = req.params;
+
+    const userId: number = parseInt(id);
+
+    if (!id || isNaN(userId)) {
+      throw new BadRequestError(chat.error400);
+    }
+
+    const user: User | null = await userRepository.findOne({ where: { id: userId } });
+
+    if (!user) {
+      throw new NotFoundError(chat.error404);
+    }
+    return res.status(200).json(user);
+  }
+
+  async login(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      throw new InvalidFormatError(chat.error400);
+    }
+
+    const users: User[] = await userRepository.findByEmail(String(email));
+
+    if (users.length < 1) {
+      throw new NotFoundError(chat.error400);
+    }
+
+    const user: User = users[0];
+
+    const correctPassword: boolean = await bcrypt.compare(password, user.password);
+
+    if (!correctPassword) {
+      throw new InvalidFormatError(chat.error400);
+    }
+
+    const jwtPass: string | undefined = process.env.JWT_PASS;
+
+    if (!jwtPass) {
+      throw new ServerError('JWT_PASS environment variable is not set');
+    }
+
+    const token = jwt.sign({ id: user.id }, jwtPass, { expiresIn: '8h' });
+
+    const { password: _, ...userData } = user;
+
+    return res.status(200).json({ user: userData, token });
+  }
+
+  async update(req: Request, res: Response) {}
+
+  async destroy(req: RequestWhitEntity, res: Response) {
+    const { id } = req.params;
+
+    const userId: number = parseInt(id);
+
+    if (!id || isNaN(userId)) {
+      throw new InvalidFormatError(chat.error400);
+    }
+
+    const user: User | null = await userRepository.findOne({ where: { id: userId } });
+
+    if (!user) {
+      throw new NotFoundError(chat.error404);
+    }
+
+    const avatar: string | undefined = user.avatar;
+
+    if (avatar) {
+      await deleteImage(avatar, 'profiles/users');
+    }
+    if (user.id === req.user.id) {
+      await userRepository.delete(user);
+    } else {
+      throw new UnauthorizedError(chat.error401);
+    }
+
+    return res.status(203).json();
+  }
+}
