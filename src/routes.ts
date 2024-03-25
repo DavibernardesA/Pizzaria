caa34f9c84e4a6baa755f074b108d65934ab0d9f
Davi <lobpreal@gmail.com>
2024-03-24 22:12:48 -0300

    added route controllers

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,27 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
+import { UserController } from './controllers/userController';
+import multer from './middlewares/multer';
+import { EmployeeController } from './controllers/employeeController';
+import { loggedInEmployee } from './middlewares/loggedInEmployee';
+import { loggedInUser } from './middlewares/loggedInUser';
 
 const routes: Router = Router();
+const userController: UserController = new UserController();
+const employeeController: EmployeeController = new EmployeeController();
 
-routes.get('/', (req: Request, res: Response) => res.status(200).json('Hello, World!'));
+//employees
+routes.put('/employees', multer.single('profile_image'), employeeController.store);
+routes.post('/employees', employeeController.login);
+routes.get('/employees', loggedInEmployee, employeeController.index);
+routes.get('/employees/:id', employeeController.show);
+routes.put('/employees/:id', loggedInEmployee, employeeController.update);
+routes.delete('/employees/:id', loggedInEmployee, employeeController.destroy);
+
+//users
+routes.put('/users', multer.single('profile_image'), userController.store);
+routes.post('/users', userController.login);
+routes.get('/users/:id', loggedInUser, userController.show);
+routes.put('/users/:id', loggedInUser, userController.update);
+routes.delete('/users/:id', loggedInUser, userController.destroy);
 
 export default routes;
