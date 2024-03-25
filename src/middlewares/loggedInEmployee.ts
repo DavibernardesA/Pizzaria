903871c492ef70a16879cc4f6af4ac4d37f614fd
Davi <lobpreal@gmail.com>
2024-03-24 22:14:39 -0300

    added login checker

---
+++ src/middlewares/loggedInEmployee.ts
@@ -0,0 +1,33 @@
+import { Response, NextFunction } from 'express';
+import { UnauthorizedError } from '../helpers/api-error';
+import chat from '../chat/statusMessage';
+import jwt from 'jsonwebtoken';
+import { JwtPayload } from 'jsonwebtoken';
+import { envChecker } from '../utils/envChecker';
+import { employeeRepository } from '../repositories/employeeRepository';
+import { Employee } from '../entities/Employee';
+import { RequestWhitEntity } from '../interfaces/RequestWhitEntity';
+
+export const loggedInEmployee = async (req: RequestWhitEntity, res: Response, next: NextFunction): Promise<void> => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    throw new UnauthorizedError(chat.error401);
+  }
+
+  const token: string = authorization.split(' ')[1];
+
+  const jwtPass: string = envChecker(process.env.JWT_PASS);
+
+  const { id } = jwt.verify(token, jwtPass) as JwtPayload;
+
+  const employee: Employee | null = await employeeRepository.findOne({ where: { id } });
+
+  if (!employee) {
+    throw new UnauthorizedError(chat.error401);
+  }
+
+  req.user = employee;
+
+  next();
+};
---
+++ src/middlewares/loggedInUser.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from 'express';
+import { UnauthorizedError } from '../helpers/api-error';
+import chat from '../chat/statusMessage';
+import jwt from 'jsonwebtoken';
+import { JwtPayload } from 'jsonwebtoken';
+import { envChecker } from '../utils/envChecker';
+import { userRepository } from '../repositories/userRepository';
+import { User } from '../entities/User';
+import { RequestWhitEntity } from '../interfaces/RequestWhitEntity';
+
+export const loggedInUser = async (req: RequestWhitEntity, res: Response, next: NextFunction): Promise<void> => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    throw new UnauthorizedError(chat.error401);
+  }
+
+  const token: string = authorization.split(' ')[1];
+
+  const jwtPass: string = envChecker(process.env.JWT_PASS);
+
+  const { id } = jwt.verify(token, jwtPass) as JwtPayload;
+
+  const user: User | null = await userRepository.findOne({ where: { id } });
+
+  if (!user) {
+    throw new UnauthorizedError(chat.error401);
+  }
+
+  req.user = user;
+
+  next();
+};
