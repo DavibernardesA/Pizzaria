271e968afc85de2026723ecfbb082d0694db1d01
Davi <lobpreal@gmail.com>
2024-03-31 12:12:51 -0300

    address validation implemented

--- src/middlewares/validateAdress.ts
+++ src/middlewares/validateAdress.ts
@@ -2,20 +2,41 @@ import { NextFunction, Request, Response } from 'express';
 import { InvalidFormatError, ServerError } from '../helpers/api-error';
 import axios from 'axios';
 import chat from '../chat/statusMessage';
+import { userRepository } from '../repositories/userRepository';
+import { Address } from '../entities/Address';
+import { User } from '../entities/User';
 
 export const validateAdress = async (req: Request, res: Response, next: NextFunction) => {
-  const address: string = req.body.address;
+  const { id } = req.params;
+
+  const userId: number = parseInt(id);
+
+  const user: User | null = await userRepository.findOne({
+    where: { id: userId },
+    relations: {
+      address: true
+    }
+  });
+
+  if (!user) {
+    throw new InvalidFormatError(chat.error400);
+  }
+
+  const address: Address | undefined = user.address;
 
   if (!address) {
     throw new InvalidFormatError(chat.error400);
   }
-  console.log('endereço válido.');
+
+  const stringAdress: string = `${address.street}, house n°${address.house_number}, ${address.neighborhood}, ${address.city} - ${address.state}, zipcode: ${address.zipcode}.`;
+
+  console.log(`Valid Address: ${stringAdress}`);
   next();
   // const pizzaAddress: string = process.env.ZIP_PIZZA || '';
   // const distanceFromOrder = (process.env.ORDER_IN_KM as number | undefined) || 7;
 
   // // Requesting Google Maps Geocoding API to get the coordinates of the address
-  // const encodedAddress: string = encodeURIComponent(address);
+  // const encodedAddress: string = encodeURIComponent(stringAdress);
   // const geocodingUrl: string = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${process.env.API_KEY_MAPS}`;
 
   // const geocodingResponse = await axios.get(geocodingUrl);
