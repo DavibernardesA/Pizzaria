2d210a1f44291fb59e6e5a89e9a9c5a176ee01e2
Davi <lobpreal@gmail.com>
2024-03-31 22:56:00 -0300

    added error message without address

--- src/chat/statusMessage.ts
+++ src/chat/statusMessage.ts
@@ -6,7 +6,8 @@ const chat = {
   error403: 'Access denied: you do not have permission to access this resource.',
   error404: 'The server cannot find the requested resource.',
   error429: 'Request limit exceeded. Please try again later.',
-  error500: 'Internal server error.'
+  error500: 'Internal server error.',
+  noAddress: 'You need to have an address linked to your account to place an order.'
 };
 
 export default chat;
