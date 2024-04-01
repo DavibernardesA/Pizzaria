b8799890f7334e5eda16e65bd4c37399a908d300
Davi <lobpreal@gmail.com>
2024-03-31 21:08:28 -0300

    improved route typing and added product CRUD routes

--- src/routes.ts
+++ src/routes.ts
@@ -4,25 +4,35 @@ import multer from './middlewares/multer';
 import { EmployeeController } from './controllers/employeeController';
 import { loggedInEmployee } from './middlewares/loggedInEmployee';
 import { loggedInUser } from './middlewares/loggedInUser';
+import { validateAdress } from './middlewares/validateAdress';
+import { ProductController } from './controllers/ProductController';
 
 const routes: Router = Router();
 const userController: UserController = new UserController();
 const employeeController: EmployeeController = new EmployeeController();
+const productController: ProductController = new ProductController();
 
 //employees
-routes.put('/employees', multer.single('profile_image'), employeeController.store);
-routes.post('/employees', employeeController.login);
+routes.post('/employees', multer.single('profile_image'), employeeController.store);
+routes.post('/employees/login', employeeController.login);
 routes.get('/employees', loggedInEmployee, employeeController.index);
 routes.get('/employees/:id', employeeController.show);
 routes.put('/employees/:id', loggedInEmployee, multer.single('profile_image'), employeeController.update);
 routes.delete('/employees/:id', loggedInEmployee, employeeController.destroy);
 
 //users
-routes.put('/users', multer.single('profile_image'), userController.store);
-routes.post('/users', userController.login);
+routes.post('/users', multer.single('profile_image'), userController.store);
+routes.post('/users/login', userController.login);
 routes.get('/users/:id', loggedInUser, userController.show);
 routes.put('/users/:id', loggedInUser, multer.single('profile_image'), userController.update);
 routes.delete('/users/:id', loggedInUser, userController.destroy);
-routes.post('/users/:id', loggedInUser, userController.create_order);
+routes.post('/order/:id', loggedInUser, validateAdress, userController.create_order);
+
+//products
+routes.get('/products', productController.index);
+routes.get('/products/:id', productController.show);
+routes.post('/products', loggedInEmployee, productController.store);
+routes.put('/products/:id', loggedInEmployee, productController.update);
+routes.delete('/products/:id', loggedInEmployee, productController.destroy);
 
 export default routes;
